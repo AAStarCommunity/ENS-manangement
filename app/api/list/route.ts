c29f95ecba589351e4b0a6fdc3055d6c60c337aa
Neal <1107554021@qq.com>
2024-06-02 19:56:50 +0800

    feat: delete some code

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,15 +1,9 @@
 import { getAll } from '@/lib/utils/query';
 
 export async function GET(request: Request) {
-    // const { searchParams } = new URL(request.url)
-    // const id = searchParams.get('id')
-    // console.log(id);
-
     let res;
     try {
         res = await getAll();
-        console.log(res);
-
     } catch (e) {
         res = []
     }
