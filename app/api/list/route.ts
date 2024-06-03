72c90ac488c3ffd5e63638be642321c8eeb13371
Neal <1107554021@qq.com>
2024-06-04 02:31:36 +0800

    feat: update cached time

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -5,7 +5,7 @@ export const revalidate = 10;
 export async function GET(request: Request) {
     let res;
     try {
-        res = await getAll({ ttl: 10, swr: 10 });
+        res = await getAll({ ttl: 0, swr: 0 });
     } catch (e) {
         res = []
     }
